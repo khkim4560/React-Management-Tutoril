e2b1def1d71edfba681f6c5e177ad3241bd1bd8e
kim hyunki <khkim4560@gmail.com>
2022-12-07 15:26:31 +0900

    Add Material UI Design for Add/Delete

--- client/src/components/CustomerDelete.js
+++ client/src/components/CustomerDelete.js
@@ -1,13 +1,21 @@
 import React from "react";
 import axios from 'axios'
-import { Button } from "@material-ui/core";
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import Button  from "@material-ui/core/Button";
+import Typography  from "@material-ui/core/Typography";
+
 const apiUrl = "http://localhost:5000/api/customers";
 
 class CustomerDelete extends React.Component{
+    
     constructor(props){
         super(props);
         this.state ={
-            id : this.props.id
+            id : this.props.id,
+            open: false
         }
     }
 
@@ -35,16 +43,46 @@ class CustomerDelete extends React.Component{
         .catch(err => console.log("deleteCustomer ERR",err));
     }
 
+
+     //Modal Open
+     handleClickOpen =  () =>{        
+        this.setState({open : true});
+    }
+
+    //Modal Close
+    handleClickClose =  () =>{
+        this.setState(
+            this.state ={            
+            open        : false
+        });            
+    }
+
+
     render(){
         return(
-            
-            <Button style={{backgroundColor:"red",color: "yellow" }} onClick={this.handleDeleteClick}>삭제</Button>    
-            
+            <div>
+            <Button variant="contained" colr="secondary" onClick={this.handleClickOpen}>삭제</Button>    
+
+            <Dialog open={this.state.open} onClose={this.handleClickClose}>
+                    <DialogTitle onClose={this.handleClickClose} >
+                        삭제경고
+                     </DialogTitle>
+                    
+                    <DialogContent>                                    
+                        <Typography gutterBottom>
+                         선택한 고객 정보가 삭제됩니다.
+                        </Typography>
+                    </DialogContent >
+                    
+                    <DialogActions>
+                        <Button variant="contained" color="primary" onClick={this.deleteCustomer}>삭제</Button>                
+                        <Button variant="outlined"  color="primary" onClick={this.handleClickClose}>닫기</Button>                    
+                    </DialogActions>
+                </Dialog>
+            </div>   
         )
     }
 
 }
 
 export default CustomerDelete;
-
-
